0a8b47a2fbd6d64c9403f3e05e22ff72099c8e92
pconesa <p.conesa.mingo@gmail.com>
2014-10-28 16:38:28 +0000

    do a simple mvn package deploy, change deploy repository.

--- import_layer/package.sh
+++ import_layer/package.sh
@@ -4,20 +4,10 @@
 
 #MVNOPTS="--offline"
 
-ISAversion=1.6.9
-MVN=/usr/local/apache-maven-2.2.1/bin/mvn
+#MVN=/usr/local/apache-maven-2.2.1/bin/mvn
 #MVN=/usr/bin/mvn
 
 cd ../import_layer
-$MVN $MVNOPTS -Ptools,build_cmd_deps -Dmaven.test.skip=true clean package
+mvn $MVNOPTS -Dmaven.test.skip=true clean package
 
-# This is necessary due to some wrong packaging in one of our dependencies
-# Final jar won't work unless you remove this silly file
-#
-zip target/import_layer_deps_${ISAversion}.jar -d '\*.class'
-
-$MVN install:install-file -Dfile=target/import_layer_deps_${ISAversion}.jar -DgroupId=org.isatools -DartifactId=import_layer_MTBLS -Dversion=${ISAversion}-SNAPSHOT -Dpackaging=jar -DuniqueVersion=false
-$MVN deploy:deploy-file -Dfile=target/import_layer_deps_${ISAversion}.jar -DgroupId=org.isatools -DartifactId=import_layer_MTBLS -Dversion=${ISAversion}-SNAPSHOT -Dpackaging=jar -DrepositoryId=chebi-repo2 -Durl=scp://prod.ebi.ac.uk/ebi/sp/pro1/chebi/maven2_repo -DuniqueVersion=false
-
-# ssh the file to the cluster:
-# mvn deploy:deploy-file -Dfile=ISAcreator-1.7.3-SNAPSHOT.jar -DgroupId=org.isatools -DartifactId=ISAcreator -Dversion=1.7.3-SNAPSHOT -Dpackaging=jar -DrepositoryId=ebi-001.ebi.ac.uk -Durl=file:/ebi/sp/pro1/chebi/maven2_repo
+mvn deploy
